0b86684f5a82e3675dfbdcea1cadf3ee3fa728bd
KeweiQ <1967808106@qq.com>
2019-04-15 23:29:45 -0400

    update bitmap.c

--- Lab/lab5/bitmap.c
+++ Lab/lab5/bitmap.c
@@ -8,18 +8,20 @@
  * height in the given bitmap file.
  */
 void read_bitmap_metadata(FILE *image, int *pixel_array_offset, int *width, int *height) {
-    int error = 0;
-
     fseek(image, 10, SEEK_SET);
-    error += fread(pixel_array_offset, 4, 1, image);
+    if (fread(pixel_array_offset, 4, 1, image) != 1) {
+        fprintf(stderr, "Read bitmap metadata failed!\n");
+        exit(1);
+    }
 
     fseek(image, 18, SEEK_SET);
-    error += fread(width, 4, 1, image);
+    if (fread(width, 4, 1, image) != 1) {
+        fprintf(stderr, "Read bitmap metadata failed!\n");
+        exit(1);
+    }
 
     fseek(image, 22, SEEK_SET);
-    error += fread(height, 4, 1, image);
-
-    if (error != 3) {
+    if (fread(height, 4, 1, image) != 1) {
         fprintf(stderr, "Read bitmap metadata failed!\n");
         exit(1);
     }
@@ -47,20 +49,26 @@ struct pixel **read_pixel_array(FILE *image, int pixel_array_offset, int width, 
         bmp_image[h] = malloc(sizeof(struct pixel) * width);
     }
 
-    int error = 0;
     fseek(image, pixel_array_offset, SEEK_SET);
     for (int h = 0; h < height; h++) {
         for (int w = 0; w < width; w++) {
-            error += fread(&bmp_image[h][w].blue, 1, 1, image);
-            error += fread(&bmp_image[h][w].green, 1, 1, image);
-            error += fread(&bmp_image[h][w].red, 1, 1, image);
+            if (fread(&bmp_image[h][w].blue, 1, 1, image) != 1) {
+                fprintf(stderr, "Read bmp file failed!\n");
+                exit(1);
+            }
+
+            if (fread(&bmp_image[h][w].green, 1, 1, image) != 1) {
+                fprintf(stderr, "Read bmp file failed!\n");
+                exit(1);
+            }
+
+            if (fread(&bmp_image[h][w].red, 1, 1, image) != 1) {
+                fprintf(stderr, "Read bmp file failed!\n");
+                exit(1);
+            }
         }
     }
 
-    if (error != (height * width * 3)) {
-        fprintf(stderr, "Read bmp file failed!\n");
-        exit(1);
-    }
     return bmp_image;
 }
 
